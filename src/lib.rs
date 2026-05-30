4e6bae4676c794564dc5be6c73bfe0bc03cbdd90
Marcos Pablo <marquin.pablo@gmail.com>
2026-05-30 12:21:36 -0300

    feat(parser): add basic parser combinators and tests
    Introduce a minimal parser combinator framework with core combinators such
    as , , , and . Implement literal and identifier
    parsers, along with unit tests for each parser and combinator. Set up
    initial project files including , , and
    .

---
+++ src/lib.rs
@@ -0,0 +1,146 @@
+trait Parser<'a, Output> {
+    fn parse(&self, input: &'a str) -> ParseResult<'a, Output>;
+}
+
+type ParseResult<'a, Output> = Result<(&'a str, Output), &'a str>;
+
+impl<'a, F, Output> Parser<'a, Output> for F
+where
+    F: Fn(&'a str) -> ParseResult<Output>,
+{
+    fn parse(&self, input: &'a str) -> ParseResult<'a, Output> {
+        self(input)
+    }
+}
+
+fn map<'a, P, F, A, B>(parser: P, map_fn: F) -> impl Parser<'a, B>
+where
+    P: Parser<'a, A>,
+    F: Fn(A) -> B,
+{
+    move |input| {
+        parser
+            .parse(input)
+            .map(|(next_input, result)| (next_input, map_fn(result)))
+    }
+}
+
+fn pair<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, (R1, R2)>
+where
+    P1: Parser<'a, R1>,
+    P2: Parser<'a, R2>,
+{
+    move |input| {
+        parser1.parse(input).and_then(|(next_input, result1)| {
+            parser2
+                .parse(next_input)
+                .map(|(last_input, result2)| (last_input, (result1, result2)))
+        })
+    }
+}
+
+fn left<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R1>
+where
+    P1: Parser<'a, R1>,
+    P2: Parser<'a, R2>,
+{
+    map(pair(parser1, parser2), |(left, _right)| left)
+}
+
+fn right<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R2>
+where
+    P1: Parser<'a, R1>,
+    P2: Parser<'a, R2>,
+{
+    map(pair(parser1, parser2), |(_left, right)| right)
+}
+
+fn match_literal<'a>(expected: &'static str) -> impl Parser<'a, ()> {
+    move |input: &'a str| match input.starts_with(expected) {
+        true => Ok((&input[expected.len()..], ())),
+        false => Err(input),
+    }
+}
+
+fn identifier<'a>(input: &'a str) -> ParseResult<'a, String> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+
+    while let Some(next) = chars.next() {
+        if next.is_alphanumeric() || next == '-' {
+            matched.push(next);
+            continue;
+        }
+
+        break;
+    }
+
+    let next_index = matched.len();
+
+    Ok((&input[next_index..], matched))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn literal_parser() {
+        let parse_joe = match_literal("Hello Joe!");
+        assert_eq!(parse_joe.parse("Hello Joe!"), Ok(("", ())));
+        assert_eq!(
+            parse_joe.parse("Hello Joe! Hello Robert!"),
+            Ok((" Hello Robert!", ()))
+        );
+        assert_eq!(parse_joe.parse("Hello Mike!"), Err("Hello Mike!"));
+    }
+
+    #[test]
+    fn identifier_parser() {
+        assert_eq!(
+            identifier("i-am-an-identifier"),
+            Ok(("", "i-am-an-identifier".to_string()))
+        );
+
+        assert_eq!(
+            identifier("not entirely an identifier"),
+            Ok((" entirely an identifier", "not".to_string()))
+        );
+
+        assert_eq!(
+            identifier("!not at all an identifier"),
+            Err("!not at all an identifier")
+        );
+    }
+
+    #[test]
+    fn pair_combinator() {
+        let tag_opener = pair(match_literal("<"), identifier);
+
+        assert_eq!(
+            tag_opener.parse("<my-first-element/>"),
+            Ok(("/>", ((), "my-first-element".to_string())))
+        );
+
+        assert_eq!(tag_opener.parse("oops"), Err("oops"));
+        assert_eq!(tag_opener.parse("<!oops"), Err("!oops"));
+    }
+
+    #[test]
+    fn right_combinator() {
+        let tag_opener = right(match_literal("<"), identifier);
+
+        assert_eq!(
+            tag_opener.parse("<my-first-element/>"),
+            Ok(("/>", "my-first-element".to_string()))
+        );
+
+        assert_eq!(tag_opener.parse("oops"), Err("oops"));
+        assert_eq!(tag_opener.parse("<!oops"), Err("!oops"));
+    }
+}
